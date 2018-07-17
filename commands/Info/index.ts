e3a712a9ec5873d863e84545f0eb98ccb7ce1bab
mattmark <matt.vandusen@float-left.co>
2018-07-17 17:19:55 -0500

    feat(Module): WIP New command and action

---
+++ commands/Info/index.ts
@@ -0,0 +1,14 @@
+import { CommanderStatic } from "commander"
+import { AbstractCommand } from "../Command"
+
+export class InfoCommand extends AbstractCommand {
+  public load(program: CommanderStatic) {
+    program
+      .command("info")
+      .alias("i")
+      .description("Display Techton CLI details")
+      .action(async () => {
+        await this.action.handle()
+      })
+  }
+}
